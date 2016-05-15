f58c59d1268c0e89efd58127d91d9089ccdedc5b
Pepa <pepa.hruska@gmail.com>
2016-05-15 17:01:12 +0200

    Updated UnitTest

--- app/src/test/java/cz/johrusk/showsmscode/ExampleUnitTest.java
+++ app/src/test/java/cz/johrusk/showsmscode/ExampleUnitTest.java
@@ -12,195 +12,142 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ * Class for testing new SMS patterns
  */
+
 @RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest{
 
     static SmsParser smsParser;
 
-
     @BeforeClass
-    public static void setup() {
+    public static void setup() {smsParser = new SmsParser();}
 
-         smsParser = new SmsParser();
 
+    /**
+     * Write method for testing SMS patterns here.
+     * You just need to replace these four vars with correct Strings for new SMS.
+     */
+
+    @Test
+    public void ExampleForTesting(){
+        //SMS Body
+        String msg_content = ("Hello world. Your code is: 123456 ");
+        String unique = "Hello world";//Unique code which has to be included in SMS
+        String patern = "code is: (\\d+)";//Regular expression to find a code
+        String expectedResult = "123456";// Code which should be find
+
+        String code = SmsParser.TestSMSPattern(msg_content,unique,patern);
+        Assert.assertEquals(expectedResult, code);
     }
 
-
-
-//    @Test
-//    public void Servis24() {
-//        //android.util.Log.d("test","ServisTesStarts");
-//        Sms sms = smsParser.parse(999005,"CS-S24: Entered PLATBA 24 payment to account 0-6276462/0800; amount 6685,00 CZK; variable symbol 676856413. Authorization SMS code: 88336829");
-//        Assert.assertEquals(999005,sms.number);
-//        Assert.assertEquals("CS-S24",sms.unique);
-//        Assert.assertEquals("Česká Spořitelna",sms.sender);
-//        Assert.assertEquals("88336829",sms.code);
-//
-//
-//    }
     @Test
     public void Servis24(){
         String msg_content = ("CS-S24: Entered PLATBA 24 payment to account 0-6276462/0800; amount 6685,00 CZK; variable symbol 676856413. Authorization SMS code: 88336829");
         String unique = "CS-S24";
+        String patern = "code: (\\d+)";
+        String expectedResult = "88336829";
 
-        String code = null;
-    Pattern p = Pattern.compile("code: (\\d+)");
-    Matcher matcher = p.matcher(msg_content);
-        Pattern pUnique = Pattern.compile(unique);
-        Matcher mUnique = pUnique.matcher(msg_content);
-    while (matcher.find() && mUnique.find()) {
-        code = matcher.group(1);
-        }
-        Assert.assertEquals("88336829",code);
+        String code = SmsParser.TestSMSPattern(msg_content,unique,patern);
+        Assert.assertEquals(expectedResult, code);
     }
 
     @Test
     public void Raiffeisenbank(){
         String msg_content = ("Vazeny kliente, 3D secure kod pro Vasi internetovou transakci na castku: 100.00 CZK u obchodnika: GOPAY s.r.o. je: 820205. Vase Raiffeisenbank");
         String unique = "Raiffeisenbank";
+        String patern ="je: (\\d+)";
+        String expectedResult = "820205";
 
-        String code = null;
-        Pattern p = Pattern.compile("je: (\\d+)");
-        Matcher matcher = p.matcher(msg_content);
-        Pattern pUnique = Pattern.compile(unique);
-        Matcher mUnique = pUnique.matcher(msg_content);
-        while (matcher.find() && mUnique.find()) {
-            code = matcher.group(1);
-
-        }
-        Assert.assertEquals("820205",code);
+        String code = SmsParser.TestSMSPattern(msg_content,unique,patern);
+        Assert.assertEquals(expectedResult, code);
     }
 
     @Test
     public void CSOB(){
         String msg_content = ("CSOB SMS klic: Prihlaseni do CSOB InternetBankingu 24 (trans. 364319819) potvrdte kodem f11-x29-zpk.");
         String unique = "CSOB";
+        String patern = "kodem (...-...-...)";
+        String expectedResult ="f11-x29-zpk";
 
-        String code = null;
-        Pattern p = Pattern.compile("kodem (...-...-...)");
-        Matcher matcher = p.matcher(msg_content);
-        Pattern pUnique = Pattern.compile(unique);
-        Matcher mUnique = pUnique.matcher(msg_content);
-        while (matcher.find() && mUnique.find()) {
-            code = matcher.group(1);
-        }
-        Assert.assertEquals("f11-x29-zpk",code);
-
-
+        String code = SmsParser.TestSMSPattern(msg_content,unique,patern);
+        Assert.assertEquals(expectedResult, code);
     }
     //TODO Find out sender
     @Test
     public void MojeBanka(){
         String msg_content = ("MojeBanka: Prihlaseni do aplikace; datum a cas: 29.03.2016 21:52:19; Autorizacni SMS kod: 465 057");
         String unique = "MojeBanka";
+        String patern = "SMS kod: (... ...)";
+        String expectedResult = "465 057";
 
-        String code = null;
-        Pattern p = Pattern.compile("SMS kod: (... ...)");
-        Matcher matcher = p.matcher(msg_content);
-        Pattern pUnique = Pattern.compile(unique);
-        Matcher mUnique = pUnique.matcher(msg_content);
-        while (matcher.find() && mUnique.find()) {
-            code = matcher.group(1);
-        }
-        Assert.assertEquals("465 057",code);
+        String code = SmsParser.TestSMSPattern(msg_content,unique,patern);
+        Assert.assertEquals(expectedResult, code);
     }
 
     @Test
     public void WarGaming(){
         String msg_content = ("Confirmation code: 41366. Never give this code out. Wargaming.net");
         String unique = "Wargaming";
+        String patern = "code: (.....)";
+        String expectedResult = "41366";
 
-        String code = null;
-        Pattern p = Pattern.compile("code: (.....)");
-        Matcher matcher = p.matcher(msg_content);
-        Pattern pUnique = Pattern.compile(unique);
-        Matcher mUnique = pUnique.matcher(msg_content);
-        while (matcher.find() && mUnique.find()) {
-            code = matcher.group(1);
-        }
-        Assert.assertEquals("41366",code);
+        String code = SmsParser.TestSMSPattern(msg_content,unique,patern);
+        Assert.assertEquals(expectedResult, code);
     }
 
     @Test
     public void Blizzard(){
         String msg_content = ("Your verification code is 176015 - Blizzard Entertainment");
         String unique = "Blizzard";
+        String patern = "code is (......)";
+        String expectedResult = "176015";
 
-        String code = null;
-        Pattern p = Pattern.compile("code is (......)");
-        Matcher matcher = p.matcher(msg_content);
-        Pattern pUnique = Pattern.compile(unique);
-        Matcher mUnique = pUnique.matcher(msg_content);
-        while (matcher.find() && mUnique.find()) {
-            code = matcher.group(1);
-        }
-        Assert.assertEquals("176015",code);
+        String code = SmsParser.TestSMSPattern(msg_content,unique,patern);
+        Assert.assertEquals(expectedResult, code);
     }
-
 
     @Test
     public void GoogleAuthenticatorCZ(){
         String msg_content = ("G-549121 je váš ověřovací kód Google.");
         String unique = "Google";
+        String patern = "G-(......)";
+        String expectedResult = "549121";
 
-        String code = null;
-        Pattern p = Pattern.compile("G-(......)");
-        Matcher matcher = p.matcher(msg_content);
-        Pattern pUnique = Pattern.compile(unique);
-        Matcher mUnique = pUnique.matcher(msg_content);
-        while (matcher.find() && mUnique.find()) {
-            code = matcher.group(1);
-        }
-        Assert.assertEquals("549121",code);
+        String code = SmsParser.TestSMSPattern(msg_content,unique,patern);
+        Assert.assertEquals(expectedResult, code);
     }
 
     @Test
     public void GoogleAuthenticatorEN(){
         String msg_content = ("G-549121 is your Google verification code.");
         String unique = "Google";
+        String patern = "G-(......)";
+        String expectedResult = "549121";
 
-        String code = null;
-        Pattern p = Pattern.compile("G-(......)");
-        Matcher matcher = p.matcher(msg_content);
-        Pattern pUnique = Pattern.compile(unique);
-        Matcher mUnique = pUnique.matcher(msg_content);
-        while (matcher.find() && mUnique.find()) {
-            code = matcher.group(1);
-        }
-        Assert.assertEquals("549121",code);
+        String code = SmsParser.TestSMSPattern(msg_content,unique,patern);
+        Assert.assertEquals(expectedResult, code);
     }
 
     @Test
     public void O2Active(){
         String msg_content = ("Vazeny zakazniku, zasilame jednorazovy PIN k autorizaci m-platby pro Audioteka s.a. na castku 1.0 Kc. PIN: 777DKE. Nesdelujte jine osobe. Vase O2.");
         String unique = "m-platby";
+        String patern = "PIN: (......)";
+        String expectedResult = "777DKE";
 
-        String code = null;
-        Pattern p = Pattern.compile("PIN: (......)");
-        Matcher matcher = p.matcher(msg_content);
-        Pattern pUnique = Pattern.compile(unique);
-        Matcher mUnique = pUnique.matcher(msg_content);
-        while (matcher.find() && mUnique.find()) {
-            code = matcher.group(1);
-        }
-        Assert.assertEquals("777DKE",code);
+        String code = SmsParser.TestSMSPattern(msg_content,unique,patern);
+        Assert.assertEquals(expectedResult, code);
     }
     @Test
     public void Netflix(){
         String msg_content = ("Your Netflix verification code is 074922");
         String unique = "Netflix";
+        String patern = "code is (......)";
+        String expectedResult = "074922";
 
-        String code = null;
-        Pattern p = Pattern.compile("code is (......)");
-        Matcher matcher = p.matcher(msg_content);
-        Pattern pUnique = Pattern.compile(unique);
-        Matcher mUnique = pUnique.matcher(msg_content);
-        while (matcher.find() && mUnique.find()) {
-            code = matcher.group(1);
-        }
-        Assert.assertEquals("074922",code);
+        String code = SmsParser.TestSMSPattern(msg_content,unique,patern);
+        Assert.assertEquals(expectedResult, code);
     }
 
 }
--- app/src/test/java/cz/johrusk/showsmscode/SmsParser.java
+++ app/src/test/java/cz/johrusk/showsmscode/SmsParser.java
@@ -23,105 +23,23 @@ import cz.johrusk.showsmscode.activity.Main_activity;
  * Created by Pepa on 02.04.2016.
  */
 @RunWith(MockitoJUnitRunner.class)
-public class SmsParser{
+public class SmsParser {
     @Mock
     public Context c;
-    public static final String LOG_TAG = Main_activity.class.getName();
 
 
-    public Sms parse(long number, String smsText){
-        String[] smsContent = getSmsContent(number, smsText);
-        Sms sms = new Sms();
-        sms.unique = smsContent[0];
-        sms.sender = smsContent[1];
-        sms.number = smsContent[2];
-        sms.code = smsContent[3];
 
-        return sms;
-
-    }
-
-
-//    public String loadJSONFromAsset() {
-
-//        String json = null;
-//        try {
-//            try {
-//                InputStream is = c.getAssets().open("SMSJson.json");
-//
-//
-//            int size = is.available();
-//            byte[] buffer = new byte[size];
-//            is.read(buffer);
-//            is.close();
-//            json = new String(buffer, "UTF-8");
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//            return null;
-//        }
-//        return json;
-//        }
-//        catch (NullPointerException n){
-//            throw new RuntimeException("Null poitttnter exception");}
-//    }
-
-    private String readResult(InputStream is) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader r = new BufferedReader(new InputStreamReader(is, "utf-8"), 1000);
-        String l = null;
-        while ((l = r.readLine()) != null) {
-            sb.append(l);
+    public static String TestSMSPattern(String msg_content, String unique, String paternString) {
+        Pattern patern = Pattern.compile(paternString);
+        String code = null;
+        Matcher matcher = patern.matcher(msg_content);
+        Pattern pUnique = Pattern.compile(unique);
+        Matcher mUnique = pUnique.matcher(msg_content);
+        while (matcher.find() && mUnique.find()) {
+            code = matcher.group(1);
         }
-
-        return sb.toString();
+        return code;
     }
-
-
-
-    public String[] getSmsContent ( long number, String smsText) {
-            String[] results = new String[5];
-            InputStream is = null;
-            try {
-                is = ClassLoader.getSystemResourceAsStream("assets/SMSJson.json");
-                String strg = readResult(is);
-                JSONObject obj = new JSONObject(strg);
-                JSONArray m_jArry = obj.getJSONArray("sms");
-
-
-                for (int i = 0; i < m_jArry.length(); i++) {
-                    JSONObject jo_inside = m_jArry.getJSONObject(i);
-
-
-                    long number_value = jo_inside.getLong("number");
-                    if (number_value == number) {
-                        JSONArray alt_numbers = jo_inside.getJSONArray("alt_numbers");
-                        String[] altnumbers_value = new String[50];
-                        String code = "";
-
-                        Pattern p = Pattern.compile(jo_inside.getString("reg_ex"));
-                        Matcher m = p.matcher(smsText);
-                        while (m.find()) { // Find each match in turn; String can't do this.
-                            code = m.group(1); // Access a submatch group; String can't do this.
-                            Log.d(LOG_TAG, "code is: " + code);
-                        }
-
-
-                        results[0] = jo_inside.getString("unique");
-                        results[1] = jo_inside.getString("sender");
-                        results[2] = jo_inside.getString("number");
-                        results[3] = code;
-                        return results;
-                    }
-                }
-
-            } catch (IOException | JSONException e) {
-                Log.e("ShowSMSApp", "received sms cannot be parsed because of " + e.getMessage());
-            }
-
-        return results;
-        }
 }
-
-
 
 
